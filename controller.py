9856efb717ea0828b7bdb50349a0a35bf4ed572a
Navadeepan <deepannava114@gmail.com>
2023-12-27 20:23:25 +0530

    Implemented post, put, and delete task features

--- controller.py
+++ controller.py
@@ -1,8 +1,7 @@
-import logging
-
-from flask import jsonify
+from flask import request
 from flask_restful import Resource
 
+from dbconfig import db
 from models import Task
 
 
@@ -16,14 +15,54 @@ class GetTasks(Resource):
     @staticmethod
     def get():
         try:
-            # Fetch only the 'task' attribute from all tasks in the database
-            tasks = Task.query.with_entities(Task.task).all()
-
-            # Extract 'task' values from the query result
-            task_list = [task[0] for task in tasks]
-
-            # Return tasks as JSON response
-            return jsonify({'tasks': task_list})
+            tasks = Task.query.all()
+            tasks_list = [
+                {
+                    'task_name': task.task_name,
+                    'created_date': task.created_date.strftime('%Y-%m-%d %H:%M:%S') if task.created_date else None
+                }
+                for task in tasks
+            ]
+            return tasks_list
         except Exception as e:
-            logging.error("An error occurred: " + str(e))
-            return jsonify({'error': 'An error occurred while fetching tasks.'}), 500
+            return str(e)
+
+
+class AddTask(Resource):
+    @staticmethod
+    def post():
+        try:
+            data = request.json
+            task = Task(task_name=data.get('task_name'))
+            db.session.add(task)
+            db.session.commit()
+            return GetTasks.get(), 201
+        except Exception as e:
+            return str(e)
+
+
+class UpdateTask(Resource):
+    @staticmethod
+    def put():
+        try:
+            data = request.json
+            task_id = request.args.get('id')
+            task = Task.query.filter_by(id=task_id).first()
+            task.task_name = data['task_name']
+            db.session.commit()
+            return GetTasks.get()
+        except Exception as e:
+            return str(e)
+
+
+class DeleteTask(Resource):
+    @staticmethod
+    def delete():
+        try:
+            task_id = request.args.get('id')
+            task = Task.query.filter_by(id=task_id).first()
+            db.session.delete(task)
+            db.session.commit()
+            return GetTasks.get()
+        except Exception as e:
+            return str(e)
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 
-from controller import (GetTasks, Welcome)
+from controller import (GetTasks, Welcome, AddTask, UpdateTask, DeleteTask)
 
 
 def generate_routes(app):
@@ -8,3 +8,6 @@ def generate_routes(app):
     api = Api(app)
     api.add_resource(Welcome, '/')
     api.add_resource(GetTasks, '/tasks')
+    api.add_resource(AddTask, '/tasks/add')
+    api.add_resource(UpdateTask, '/tasks/update')
+    api.add_resource(DeleteTask, '/tasks/delete')
